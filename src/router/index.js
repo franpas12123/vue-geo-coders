6037d7fc203094791d6b2162f94059137380372d
Frances D Pasulohan <franpas12123@gmail.com>
2020-02-08 09:46:14 +0800

    update router

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,21 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Welcome from '@/components/Welcome';
+import Chat from '@/components/Chat/Chat';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Home
+    name: 'welcome',
+    component: Welcome
+  },
+  {
+    path: '/chat',
+    name: 'chat',
+    component: Chat,
+    props: true
   },
   {
     path: '/about',
@@ -16,14 +23,15 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
-]
+];
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
-})
+});
 
-export default router
+export default router;
