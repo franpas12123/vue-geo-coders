947760fac49ee7b304304d03759520da21de8a24
Frances D Pasulohan <franpas12123@gmail.com>
2020-02-08 09:49:46 +0800

    added welcome component to add user's name

---
+++ src/components/Welcome.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-row justify="center" style="margin-left: 0px">
+    <v-col cols="10" xl="4" lg="6" md="8">
+      <v-card>
+        <v-card-title class="justify-center">
+          <span class="display-1 mt-3">Welcome</span>
+        </v-card-title>
+        <v-card-text>
+          <v-row justify="center">
+            <v-col cols="10">
+              <v-text-field
+                color="success"
+                name="name"
+                label="Enter your name"
+                id="id"
+                v-model="name"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+        </v-card-text>
+        <v-card-actions class="justify-center">
+          <v-btn class="mb-3" color="success" @click="submit">Join Chat</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "Welcome",
+  data() {
+    return {
+      name: ""
+    };
+  },
+  methods: {
+    submit: function() {
+      alert("name: " + this.name);
+      this.$router.push({ name: "chat", params: { name: this.name } });
+    }
+  }
+};
+</script>
+
+<style>
+</style>
