bcc6fd15c0fb439d5f4595183ffa4af6da40471e
Frances D Pasulohan <franpas12123@gmail.com>
2020-02-08 09:49:06 +0800

    added content for wrapper

---
+++ src/layout/content.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container fill-height fluid>
+    <Login v-if="!loggedIn" />
+    <Welcome />
+  </v-container>
+</template>
+
+<script>
+import Login from "@/views/Login";
+import Welcome from "@/components/Welcome";
+
+export default {
+  name: "Content",
+
+  components: {
+    Welcome,
+    Login
+  },
+
+  data: () => ({
+    loggedIn: true
+  }),
+  created() {}
+};
+</script>
