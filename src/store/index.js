152da3b6b86fb513ccfa42ff7459b5938cceeba2
Frances D Pasulohan <franpas12123@gmail.com>
2020-03-10 18:58:45 +0800

    added login actions and mutation; added get user getter

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,24 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    user: ''
+  },
+  getters: {
+    getUser: state => state.user
   },
   mutations: {
+    login: (state, user) => {
+      state.user = user;
+    }
   },
   actions: {
+    login({ commit }, user) {
+      commit('login', user);
+    }
   },
-  modules: {
-  }
-})
+  modules: {}
+});
