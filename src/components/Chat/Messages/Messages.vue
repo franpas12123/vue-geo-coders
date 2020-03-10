beb0da20f364e7d30fc3f6c0ece344c25cbfcc43
Frances D Pasulohan <franpas12123@gmail.com>
2020-03-10 19:02:48 +0800

    added feature to have different styles to show user messages

--- src/components/Chat/Messages/Messages.vue
+++ src/components/Chat/Messages/Messages.vue
@@ -1,21 +1,44 @@
 <template>
-  <v-list-item>
-    Message
-    <v-list-item-avatar>
+  <v-list-item
+    class="mb-2"
+    :class="[getUser === name ? 'justify-end' : 'justify-start']"
+  >
+    <!-- <v-list-item> -->
+    <!-- Message -->
+    <v-list-item-avatar v-if="getUser != name" size="36">
       <v-img>
         <v-icon>mdi-account</v-icon>
       </v-img>
     </v-list-item-avatar>
-    <v-list-item-content v-text="msg"></v-list-item-content>
+    <!-- <v-list-item-content v-text="date"></v-list-item-content> -->
+    <!-- <v-spacer v-if="getUser === name"></v-spacer> -->
+    <v-list-item-content
+      class="message"
+      v-text="content"
+      :class="[getUser === name ? 'white--text primary' : 'white--text grey']"
+    ></v-list-item-content>
+    <!-- <v-list-item-subtitle
+      class=""
+      :class="[getUser === name ? 'white--text primary' : 'white--text grey']"
+      >{{ date }}</v-list-item-subtitle
+    > -->
   </v-list-item>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
 export default {
-  name: "Messages",
-  props: ["msg"]
+  name: 'Messages',
+  props: ['content', 'date', 'name'],
+  computed: {
+    ...mapGetters(['getUser'])
+  }
 };
 </script>
 
 <style>
+.message {
+  padding: 14px;
+}
 </style>
