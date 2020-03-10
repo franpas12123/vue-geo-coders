59a739ae7e116afabaa63cf87474cdac72cbd7a5
Frances D Pasulohan <franpas12123@gmail.com>
2020-03-10 19:01:18 +0800

    added feature to add a new message to firebase

--- src/components/Chat/Messages/SendMessage.vue
+++ src/components/Chat/Messages/SendMessage.vue
@@ -1,9 +1,9 @@
 <template>
   <v-container class="ml-3 mr-3 mt-n3 mb-n3">
-    SendMessage
+    <!-- SendMessage -->
     <v-form>
       <v-row>
-        <v-textarea rows="1" label="Type a message..." name="message" v-model="message"></v-textarea>
+        <v-textarea rows="1" label="Type a message..." name="message" v-model="newMessage"></v-textarea>
         <v-btn class="mt-2 ml-2" fab small color="success" @click="send">
           <v-icon>mdi-send</v-icon>
         </v-btn>
@@ -13,24 +13,35 @@
 </template>
 
 <script>
+import { firestore } from "@/firebase/firebase";
+
 export default {
   name: "SendMessage",
   props: ["name"],
   data() {
     return {
-      message: ""
+      newMessage: ""
     };
   },
   methods: {
     send: function() {
-      if (this.message) {
-        alert(`name: ${this.name}\nmsg: ${this.message}\n${Date.now()}`);
-        this.message = "";
+      if (this.newMessage) {
+        firestore
+          .collection("messages")
+          .add({
+            content: this.newMessage,
+            name: this.name,
+            timestamp: Date.now()
+          })
+          .catch(err => {
+            alert(err);
+          });
+        alert(`name: ${this.name}\nmsg: ${this.newMessage}\n${Date.now()}`);
+        this.newMessage = "";
       }
     }
   }
 };
 </script>
 
-<style>
-</style>
+<style></style>
